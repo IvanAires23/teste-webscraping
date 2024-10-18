271fa1ba0ede6307f41de9a75afceabaeaea0211
IvanAires23 <ivanaires32@gmail.com>
2024-10-18 16:54:51 -0300

    first

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { scrapeWebsite } from './scraper.js';
+
+const app = express();
+
+app.get('/scrape', async (req, res) => {
+    try {
+        const result = await scrapeWebsite('https://resultados.laboratorioplatano.com.br/shift/lis/platano/elis/s01.iu.web.Login.cls');
+        res.json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erro ao fazer o scraping');
+    }
+});
+
+export default app
---
+++ src/scraper.js
@@ -0,0 +1,51 @@
+import puppeteer from 'puppeteer';
+
+export async function scrapeWebsite(url) {
+    try {
+
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+
+        const username = 'P2265468'
+
+        const password = 'MY315'
+
+        await page.goto(url);
+
+        await page.type('#control_40', username);
+
+        await page.type('#control_42', password);
+
+        await page.click('#control_51');
+
+        // Espera que a navegação pós-login aconteça
+        await page.waitForNavigation({ waitUntil: 'networkidle2' });
+
+        await new Promise(resolve => setTimeout(resolve, 5000));
+        // Espera até que o botão com id "control_80" apareça
+        await page.waitForSelector('#control_54');
+        await page.click('#control_54');
+        // Espera 2 segundos
+        await new Promise(resolve => setTimeout(resolve, 5000));
+
+        await page.click('#imprimir a'); // Clica na tag <a> dentro da div "imprimir"
+
+        // Espera pela nova aba
+        const newPagePromise = new Promise(resolve => browser.once('targetcreated', target => resolve(target.page())));
+        const newPage = await newPagePromise;
+
+        // Espera a nova página carregar (ajuste conforme necessário)
+        await newPage.waitForNavigation({ waitUntil: 'networkidle2' });
+
+        // Obtém o link do PDF gerado
+        const pdfLink = newPage.url(); // Captura a URL da nova aba
+        console.log('Link do PDF:', pdfLink);
+
+
+        //await browser.close();
+        console.log('Login realizado com sucesso!');
+    } catch (error) {
+        console.error('Erro ao fazer scraping:', error);
+        throw error;
+    }
+}
